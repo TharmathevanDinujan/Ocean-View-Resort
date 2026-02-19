552013b6267b948fa085947955a9ecf925749f79
TharmathevanDinujan <146045206+TharmathevanDinujan@users.noreply.github.com>
2026-02-19 09:42:14 +0530

    Implement DBConnection utility and JDBC driver setup

---
+++ src/main/java/com/oceanview/util/DBConnection.java
@@ -0,0 +1,20 @@
+package com.oceanview.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ocean_view_resort?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USERNAME = "root"; // USER: Update if different
+    private static final String PASSWORD = "root"; // USER: Update if different
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
